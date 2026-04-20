b882c2208b96c0effafa26727baa55c1dee52a76
Mako Mohamed Aden <makomed85@gmail.com>
2026-04-20 13:37:54 +0200

    Change initial sidebar state to 'auto'

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.set_page_config(
     page_title="CineMatch — Recommandation de Films",
     page_icon="🎬",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="auto",
 )
 
 # ─────────────────────────────────────────────
